80ddefb9b1916ae580f3a5311b10103b2d10ce8d
Sondre Øvermo <sondreovermo@Sondre-sin-MacBook-Pro.local>
2026-05-24 16:04:10 +0200

    fix: completely remove service worker and offline files to resolve Safari loading hangs

--- public/js/main.js
+++ public/js/main.js
@@ -705,12 +705,25 @@ document.addEventListener('DOMContentLoaded', () => {
   })();
 
   if ('serviceWorker' in navigator) {
-
-
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(() => console.log('sw.js registrert.'))
-        .catch(err => console.error('sw.js feilet:', err));
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        for (let registration of registrations) {
+          registration.unregister().then(() => {
+            console.log('Stuck Service Worker avregistrert.');
+          });
+        }
+      });
+      
+      // Clean up all cached assets
+      if ('caches' in window) {
+        caches.keys().then(keys => {
+          keys.forEach(key => {
+            caches.delete(key).then(() => {
+              console.log('Cache tømt:', key);
+            });
+          });
+        });
+      }
     });
   }
 });
